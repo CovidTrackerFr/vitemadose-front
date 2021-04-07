c3541a583a8683a509248fd15dd3ec5d8dfdabbe
fcamblor <fcamblor@gmail.com>
2021-04-07 23:57:41 +0200

    hiding 'A propos' link

--- src/vmd-app.component.ts
+++ src/vmd-app.component.ts
@@ -37,9 +37,11 @@ export class VmdAppComponent extends LitElement {
                 </div>
                 <div class="col">
                     <div class="row justify-content-end">
+                      <!--
                         <div class="col-auto">
                             <a href="">A propos</a>
                         </div>
+                      -->
                         <div class="col-auto border-start">
                             <a href="https://covidtracker.fr/" target="_blank">CovidTracker&nbsp;<i class="bi bi-arrow-up-right"></i></a>
                         </div>
