946731bac82cdad75e8c0607fc763de69048ec41
aguy <alexandre.guy@4sh.fr>
2021-04-08 09:51:27 +0200

    Fix (UI-RDV) : Fix icon layout on rdv page. Update icon to be less button style.

--- src/views/vmd-rdv.view.ts
+++ src/views/vmd-rdv.view.ts
@@ -93,8 +93,8 @@ export class VmdRdvView extends LitElement {
             
             <div class="p-5 text-dark bg-light rounded-3">
                 <h5 class="row align-items-center justify-content-center mb-5">
-                    <i class="bi bi-check-circle-fill text-success me-2 fs-3 col-auto"></i>
-                    <span class="col-auto">
+                    <i class="bi bi-calendar-check-fill text-success me-2 fs-3 col-auto"></i>
+                    <span class="col col-sm-auto">
                         ${this.centresParDepartement?.centresDisponibles.length || 0} Centres(s) ayant des disponibilités
                     </span>
                 </h5>
@@ -135,8 +135,8 @@ export class VmdRdvView extends LitElement {
                 <div class="spacer mt-5 mb-5"></div>
 
                 <h5 class="row align-items-center justify-content-center mb-5">
-                    <i class="bi bi-x-circle-fill text-black-50 me-2 fs-3 col-auto"></i>
-                    <span class="col-auto text-black-50">
+                    <i class="bi bi-calendar-x-fill text-black-50 me-2 fs-3 col-auto"></i>
+                    <span class="col col-sm-auto text-black-50">
                         Autres centres sans créneaux de vaccination détecté
                     </span>
                 </h5>
