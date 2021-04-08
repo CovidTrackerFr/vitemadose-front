545b99b434e792741e06a0aa7c3278268b14be1c
Florent Jaby <florent.jaby@gmail.com>
2021-04-08 23:34:26 +0200

    fix horizontal scroll

--- src/views/vmd-home.view.ts
+++ src/views/vmd-home.view.ts
@@ -42,15 +42,15 @@ export class VmdHomeView extends LitElement {
                 <h1 class="searchDose-title">
                     Trouvez une dose de vaccin <span class="text-secondary">facilement</span> et <span class="text-primary">rapidement</span>
                 </h1>
-                
+
                 <div class="searchDose-form">
                     <div class="searchDoseForm-fields row align-items-center">
                       <!--
                         <div class="col-sm-24 col-md-auto mb-md-3">
-                            J'ai 
+                            J'ai
                         </div>
                         <div class="col">
-                            <vmd-tranche-age-selector class="mb-3" 
+                            <vmd-tranche-age-selector class="mb-3"
                                   @tranche-age-changed="${(event: CustomEvent<TrancheAgeSelectionne>) => this.codeTrancheAgeSelectionne = event.detail.trancheAge?.codeTrancheAge}"
                                   .tranchesAge="${TRANCHES_AGE}"
                             >
@@ -61,7 +61,7 @@ export class VmdHomeView extends LitElement {
                             Mon département :
                         </div>
                         <div class="col">
-                            <vmd-departement-selector class="mb-3" 
+                            <vmd-departement-selector class="mb-3"
                                   @departement-changed="${(event: CustomEvent<DepartementSelected>) => this.codeDepartementSelectionne = event.detail.departement?.code_departement}"
                                   .departementsDisponibles="${this.departementsDisponibles}"
                             >
@@ -76,7 +76,7 @@ export class VmdHomeView extends LitElement {
                     </div>
                 </div>
             </div>
-            
+
             <div class="searchAppointment mt-5">
                 <h5 class="text-black-50 text-center mb-5">Trouvez vos rendez-vous avec</h5>
 
@@ -90,14 +90,14 @@ export class VmdHomeView extends LitElement {
                   })}
                 </div>
             </div>
-            
+
             <div class="spacer mt-5 mb-5"></div>
-            
-            <div class="row g-5">
+
+            <div class="row">
                 <div class="col-sm-24 col-md">
                     <div class="p-5 text-dark bg-light rounded-3">
                         <h2>VaccinTracker</h2>
-                        
+
                         <p>
                             Combien de personnes ont été vaccinées ? Suivez la campagne vaccinale sur VaccinTracker.
                         </p>
@@ -112,11 +112,11 @@ export class VmdHomeView extends LitElement {
                 <div class="col-sm-24 col-md">
                     <div class="p-5 text-dark bg-light rounded-3">
                         <h2>Carte des centres</h2>
-                        
+
                         <p>
                             Trouvez un centre de vaccination directement sur la carte, appelez les centres pour savoir il y a des rendez-vous.
                         </p>
-                        
+
                         <div class="row justify-content-center mt-5">
                             <a href="/centres" class="col-auto btn btn-primary btn-lg">
                                 Accéder à la carte des centres&nbsp;<i class="bi bi-arrow-up-right"></i>
@@ -125,7 +125,7 @@ export class VmdHomeView extends LitElement {
                     </div>
                 </div>
             </div>
-            
+
             <h1>L'outil Vite Ma Dose</h1>
             <h2>Quel est l'intérêt de Vite Ma Dose de Vaccin ?</h2>
             L'outil Vite Ma Dose de Vaccin permet de trouver facilement et rapidement un rendez-vous de vaccination proche de chez soi (proche de son domicile, ou de son lieu de travail). Il permet de chercher l'ensemble des rendez-vous de vaccination possibles via les plateformes de réservation comme Doctolib, Maiia, Keldoc ou Ordoclic.
@@ -148,14 +148,6 @@ export class VmdHomeView extends LitElement {
             <p>Vite Ma Dose est une plateforme où on peut réserver un rendez-vous de vaccination. Vite Ma Dose ne collecte aucune données personnelles.</p>
 
 
-            <div class="row fs-6 legals">
-                <p class="col-sm-24 col-lg-12">
-                  Vite Ma Dose! est un outil de CovidTracker permettant de détecter les rendez-vous 
-                  disponibles dans votre département afin de vous faire vacciner (sous réserve d'éligibilité).
-                  Pour l'instant, seule les plateformes ${Object.values(PLATEFORMES).map(p => p.nom).join(", ")} sont supportées.
-                  Vite Ma Dose! n'est pas un outil officiel, n'est pas exhaustif et ne remplace pas une recherche manuelle.
-                </p>
-            </div>
 
             CovidTracker 2021 - Tous droits réservés - Merci aux contributeurs
         `;
