e434c9e7ca38bcc6eb47e3351bbe91b67d1e6425
fcamblor <fcamblor@gmail.com>
2021-04-08 23:45:15 +0200

    removed vite's base config, as it is going to be passed in github action for deployment (this will ease dist build testing locally)

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default ({ command, mode }) => ({
-  base: (mode === 'development')?'/':'/vitemadose/',
   build: {
     sourcemap: true
 
