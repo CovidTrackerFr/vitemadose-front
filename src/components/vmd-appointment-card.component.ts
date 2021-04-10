30046d9c5e2dccd0a8a0d7db55d706d32400ccca
fcamblor <fcamblor@gmail.com>
2021-04-10 11:47:33 +0200

    introduced vmd-appointment-metadata component to ease dev for metadata

--- src/components/vmd-appointment-card.component.ts
+++ src/components/vmd-appointment-card.component.ts
@@ -42,42 +42,19 @@ export class VmdAppointmentCardComponent extends LitElement {
                             <h5 class="card-title">${Dates.isoToFRDatetime(this.centre.prochain_rdv)}</h5>
                             
                             <div class="row">
-                                <!--
-                                Classes intéressantes à mettre sur les col-24 ci-dessous :
-                                - Aucune => la cellule prend toute la largeur
-                                - col-md-auto => la cellule prend la largeur du contenu
-                                - col-md => toutes les cellules en col-md sont réparties équitablement
-                                dans l'espace restant
-                                -->
-                              
-                                <div class="col-24">
-                                    <div class="row align-items-center">
-                                        <i class="bi bi-geo-alt-fill col-auto"></i>
-                                        <p class="card-text col">
-                                          ${this.centre.nom}
-                                          <br/>
-                                          ${this.centre.metadata.address}
-                                        </p>
-                                    </div>
+                              <vmd-appointment-metadata widthType="full-width" icon="bi-geo-alt-fill">
+                                <div slot="content">
+                                  ${this.centre.nom}
+                                  <br/>
+                                  ${this.centre.metadata.address}
                                 </div>
-                                ${this.centre.metadata.phone_number?html`
-                                  <div class="col-24 col-md-auto">
-                                    <div class="row align-items-center">
-                                      <i class="bi bi-telephone-fill col-auto"></i>
-                                      <p class="card-text col">
-                                        ${this.centre.metadata.phone_number}
-                                      </p>
-                                    </div>
-                                  </div>
-                                `:html``}
-                                <div class="col-24 col-md-auto">
-                                    <div class="row align-items-center">
-                                        <i class="bi bi-bag-plus col-auto"></i>
-                                        <p class="card-text col">
-                                            ${TYPES_CENTRES[this.centre.type]}
-                                        </p>
-                                    </div>
-                                </div>
+                              </vmd-appointment-metadata>
+                              <vmd-appointment-metadata widthType="fit-to-content" icon="bi-telephone-fill" .displayed="${!!this.centre.metadata.phone_number}">
+                                <span slot="content">${this.centre.metadata.phone_number}</span>
+                              </vmd-appointment-metadata>
+                              <vmd-appointment-metadata widthType="fit-to-content" icon="bi-bag-plus">
+                                <span slot="content">${TYPES_CENTRES[this.centre.type]}</span>
+                              </vmd-appointment-metadata>
                             </div>
                         </div>
                         
