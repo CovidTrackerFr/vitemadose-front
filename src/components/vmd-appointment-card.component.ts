c00cf9d5ff21f5ccb1f18f63f59b43448fba4e3b
fcamblor <fcamblor@gmail.com>
2021-04-24 03:55:20 +0200

    #73 changed the way we're displaying results, introducing phone appointments and merging both centresDisponibles and centresIndisponibles into a single list

--- src/components/vmd-appointment-card.component.ts
+++ src/components/vmd-appointment-card.component.ts
@@ -1,11 +1,13 @@
 import {css, customElement, html, LitElement, property, unsafeCSS} from 'lit-element';
 import {classMap} from "lit-html/directives/class-map";
-import {Lieu, Plateforme, PLATEFORMES, TYPES_LIEUX} from "../state/State";
+import {Lieu, LieuAffichableAvecDistance, Plateforme, PLATEFORMES, TYPES_LIEUX} from "../state/State";
 import {Router} from "../routing/Router";
 import {Dates} from "../utils/Dates";
 import appointmentCardCss from "./vmd-appointment-card.component.scss";
 import globalCss from "../styles/global.scss";
 import {Strings} from "../utils/Strings";
+import {TemplateResult} from "lit-html";
+import {styleMap} from "lit-html/directives/style-map";
 
 type LieuCliqueContext = {lieu: Lieu};
 export type LieuCliqueCustomEvent = CustomEvent<LieuCliqueContext>;
@@ -21,14 +23,7 @@ export class VmdAppointmentCardComponent extends LitElement {
         `
     ];
 
-    @property({type: Object, attribute: false}) lieu!: Lieu;
-    @property({type: Number, attribute: false}) distance!: number;
-    /* dunno why, but boolean string is not properly converted to boolean when using attributes */
-    @property({type: Boolean, attribute: false }) rdvPossible!: boolean;
-
-    private get estCliquable() {
-        return !!this.lieu.url;
-    }
+    @property({type: Object, attribute: false}) lieu!: LieuAffichableAvecDistance;
 
     constructor() {
         super();
@@ -47,21 +42,94 @@ export class VmdAppointmentCardComponent extends LitElement {
     }
 
     render() {
-        if(this.rdvPossible) {
             const plateforme: Plateforme|undefined = PLATEFORMES[this.lieu.plateforme];
-            let distance: any = this.distance
-            if (distance >= 10) {
-              distance = distance.toFixed(0)
-            } else if (distance) {
-              distance = distance.toFixed(1)
+            let distance: string|undefined;
+            if (this.lieu.distance && this.lieu.distance >= 10) {
+              distance = this.lieu.distance.toFixed(0)
+            } else if (this.lieu.distance) {
+              distance = this.lieu.distance.toFixed(1)
             }
-            return html`
-            <div class="card rounded-3 mb-5 p-4 ${classMap({clickable: this.estCliquable})}"
-                 @click="${() => this.prendreRdv()}">
+
+            let cardConfig: {cardLink:(content: TemplateResult) => TemplateResult, estCliquable: boolean, disabledBG: boolean, actions: TemplateResult|undefined, libelleDateAbsente: string };
+            if(this.lieu.url && !this.lieu.appointment_by_phone_only) {
+                let specificCardConfig: { disabledBG: boolean, libelleDateAbsente: string, libelleBouton: string, typeBouton: 'btn-info'|'btn-primary', onclick: ()=>void };
+                if(this.lieu.appointment_count === 0) {
+                    specificCardConfig = {
+                        disabledBG: true,
+                        libelleDateAbsente: 'Aucun rendez-vous',
+                        libelleBouton: 'Vérifier le centre de vaccination',
+                        typeBouton: 'btn-info',
+                        onclick: () => this.verifierRdv()
+                    };
+                } else {
+                    specificCardConfig = {
+                        disabledBG: false,
+                        libelleDateAbsente: 'Date inconnue',
+                        libelleBouton: 'Prendre rendez-vous',
+                        typeBouton: 'btn-primary',
+                        onclick: () => this.prendreRdv()
+                    };
+                }
+                cardConfig = {
+                    estCliquable: true,
+                    disabledBG: specificCardConfig.disabledBG,
+                    libelleDateAbsente: specificCardConfig.libelleDateAbsente,
+                    cardLink: (content) =>
+                        html`<a href="#" @click="${() => specificCardConfig.onclick()}">${content}</a>`,
+                    actions: html`
+                      <a href="#" class="btn btn-lg ${classMap({ 'btn-primary': specificCardConfig.typeBouton==='btn-primary', 'btn-info': specificCardConfig.typeBouton==='btn-info' })}">
+                        ${specificCardConfig.libelleBouton}
+                      </a>
+                      <div class="row align-items-center justify-content-center mt-3 text-black-50">
+                        <div class="col-auto">
+                          ${this.lieu.appointment_count.toLocaleString()} dose${Strings.plural(this.lieu.appointment_count)}
+                        </div>
+                        ${this.lieu.plateforme?html`
+                        |
+                        <div class="col-auto">
+                            ${plateforme?html`
+                            <img class="rdvPlatformLogo ${plateforme.styleCode}" src="${Router.basePath}assets/images/png/${plateforme.logo}" alt="Créneau de vaccination ${plateforme.nom}">
+                            `:html`
+                            ${this.lieu.plateforme}
+                            `}
+                        </div>
+                        `:html``}
+                      </div>
+                    `
+                };
+            } else if(this.lieu.appointment_by_phone_only && this.lieu.metadata.phone_number) {
+                cardConfig = {
+                    estCliquable: true,
+                    disabledBG: false,
+                    libelleDateAbsente: 'Réservation tél uniquement',
+                    cardLink: (content) => html`
+                          <a href="tel:${this.lieu.metadata.phone_number}"
+                             @click="${(e: Event) => e.stopImmediatePropagation()}">
+                            ${content}
+                          </a>`,
+                    actions: html`
+                          <a href="#" class="btn btn-warning btn-lg">Appeler le ${Strings.toNormalizedPhoneNumber(this.lieu.metadata.phone_number)}</a>
+                        `
+                };
+            } else {
+                cardConfig = {
+                    estCliquable: false,
+                    disabledBG: true,
+                    libelleDateAbsente: 'Aucun rendez-vous',
+                    cardLink: (content) => content,
+                    actions: undefined
+                };
+            }
+
+            return cardConfig.cardLink(html`
+            <div class="card rounded-3 mb-5 p-4 ${classMap({clickable: cardConfig.estCliquable, 'bg-disabled': cardConfig.disabledBG })}">
                 <div class="card-body">
                     <div class="row align-items-center ">
                         <div class="col">
-                            <h5 class="card-title">${Dates.isoToFRDatetime(this.lieu.prochain_rdv)}<small class="distance">${distance ? `- ${distance} km` : ''}</small></h5>
+                            <h5 class="card-title">
+                              ${this.lieu.prochain_rdv?Dates.isoToFRDatetime(this.lieu.prochain_rdv):cardConfig.libelleDateAbsente}
+                              <small class="distance">${distance ? `- ${distance} km` : ''}</small>
+                            </h5>
                             <div class="row">
                               <vmd-appointment-metadata class="mb-2" widthType="full-width" icon="vmdicon-geo-alt-fill">
                                 <div slot="content">
@@ -87,58 +155,15 @@ export class VmdAppointmentCardComponent extends LitElement {
                             </div>
                         </div>
 
-                        ${this.estCliquable?html`
+                        ${cardConfig.actions?html`
                         <div class="col-24 col-md-auto text-center mt-4 mt-md-0">
-                            <a href="#" class="btn btn-primary btn-lg">
-                              Prendre rendez-vous
-                            </a>
-                            <div class="row align-items-center justify-content-center mt-3 text-black-50">
-                                <div class="col-auto">
-                                  ${this.lieu.appointment_count.toLocaleString()} dose${Strings.plural(this.lieu.appointment_count)}
-                                </div>
-                                ${this.lieu.plateforme?html`
-                                |
-                                <div class="col-auto">
-                                    ${plateforme?html`
-                                    <img class="rdvPlatformLogo ${plateforme.styleCode}" src="${Router.basePath}assets/images/png/${plateforme.logo}" alt="Créneau de vaccination ${plateforme.nom}">
-                                    `:html`
-                                    ${this.lieu.plateforme}
-                                    `}
-                                </div>
-                                `:html``}
-                            </div>
+                          ${cardConfig.actions}
                         </div>
                         `:html``}
                     </div>
                 </div>
             </div>
-            `;
-        } else {
-            return html`
-              <div class="card rounded-3 mb-5 p-4 bg-disabled" @click="${() => this.verifierRdv()}">
-                <div class="card-body">
-                  <div class="row align-items-center">
-                    <div class="col">
-                      <h5 class="card-title">Aucun rendez-vous</h5>
-                      <vmd-appointment-metadata widthType="full-width" icon="vmdicon-geo-alt-fill">
-                        <div slot="content">
-                          <span class="fw-bold text-dark">${this.lieu.nom}</span>
-                          <br/>
-                          <em>${this.lieu.metadata.address}</em>
-                        </div>
-                      </vmd-appointment-metadata>
-                    </div>
-
-                    ${this.estCliquable?html`
-                    <div class="col-24 col-md-auto text-center mt-4 mt-md-0">
-                      <a href="#" class="btn btn-info btn-lg">Vérifier le centre de vaccination</a>
-                    </div>
-                    `:html``}
-                  </div>
-                </div>
-              </div>
-            `;
-        }
+            `);
     }
 
     connectedCallback() {
