a235fae9887f24aa0841e181ba6b705f0c3e27e5
fcamblor <fcamblor@gmail.com>
2021-05-03 19:43:43 +0200

    getting rid of some computable global variables in service-worker ... as when a push notification is triggered, those variables are undefined

--- public/sw.js
+++ public/sw.js
@@ -9,16 +9,20 @@ const VMD_BASE_URL = USE_RAW_GITHUB
     ? "https://raw.githubusercontent.com/CovidTrackerFr/vitemadose/data-auto/data/output"
     : "https://vitemadose.gitlab.io/vitemadose"
 
-let getVersionPort = undefined;
-let clientRootUrl = undefined;
-let env = undefined;
 let firebaseMessagingToken = undefined;
 let firebaseInitialized = false;
 
+function clientRootUrl() {
+    return self.location.href.replace("sw.js","");
+}
+
+function env() {
+    return (self.location.hostname === "vitemadose.covidtracker.fr")?'prod':'dev';
+}
+
+
 self.addEventListener('install', function(event) {
     console.log('Service Worker activating...');
-    clientRootUrl = event.target.location.href.replace("sw.js","");
-    env = (event.target.location.hostname === "vitemadose.covidtracker.fr")?'prod':'dev';
     // event.waitUntil(self.skipWaiting()); // Activate worker immediately
 });
 
@@ -41,7 +45,7 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener("message", function(event) {
     if (event.data && event.data.type === 'INIT_PORT') {
-        getVersionPort = event.ports[0];
+        // getVersionPort = event.ports[0];
     }
 
     if (event.data && event.data.type === 'SUBSCRIPTIONS_ADDED') {
@@ -54,7 +58,7 @@ self.addEventListener("message", function(event) {
 
 function subscribeTo(subscriptionTopics) {
     return resolveFirebaseCloudMessagingToken().then(function(fbToken) {
-        const fbBaseUrl = env==='dev'?'https://europe-west1-vite-ma-dose-dev.cloudfunctions.net':'https://europe-west1-vite-ma-dose.cloudfunctions.net';
+        const fbBaseUrl = env()==='dev'?'https://europe-west1-vite-ma-dose-dev.cloudfunctions.net':'https://europe-west1-vite-ma-dose.cloudfunctions.net';
         return Promise.all(subscriptionTopics.map(function(topic) {
             return fetch(fbBaseUrl+'/subscribeToTopic?token='+fbToken+'&topic='+topic);
         }));
@@ -63,7 +67,7 @@ function subscribeTo(subscriptionTopics) {
 
 function unsubscribeFrom(subscriptionTopics) {
     return resolveFirebaseCloudMessagingToken().then(function(fbToken) {
-        const fbBaseUrl = env==='dev'?'https://europe-west1-vite-ma-dose-dev.cloudfunctions.net':'https://europe-west1-vite-ma-dose.cloudfunctions.net';
+        const fbBaseUrl = env()==='dev'?'https://europe-west1-vite-ma-dose-dev.cloudfunctions.net':'https://europe-west1-vite-ma-dose.cloudfunctions.net';
         return Promise.all(subscriptionTopics.map(function(topic) {
             return fetch(fbBaseUrl+'/unsubscribeFromTopic?token='+fbToken+'&topic='+topic);
         }));
@@ -71,17 +75,17 @@ function unsubscribeFrom(subscriptionTopics) {
 }
 
 function resolveFirebaseCloudMessagingToken() {
-    if(!firebaseInitialized) {
-        return fetch(clientRootUrl+"firebase-config-"+env+".json")
+    if(!firebaseInitialized || !firebaseMessagingToken) {
+        return fetch(clientRootUrl()+"firebase-config-"+env()+".json")
             .then(function(resp){return resp.json(); })
             .then(function(fbConfig) {
-                firebase.initializeApp(fbConfig.app);
-                firebaseInitialized = true;
+                if(!firebaseInitialized) {
+                    firebase.initializeApp(fbConfig.app);
+                    firebaseInitialized = true;
+                }
 
                 const messaging = firebase.messaging();
                 messaging.onBackgroundMessage(function(payload) {
-                    // This is important to keep this debugger here, because putting a breakpoint into this callback will never stop otherwise
-                    // as this is kind of an off-event-loop callback which is (strangely) not inspected by chrome at the moment I'm writing this comment
                     debugger;
                     const notificationTitle = payload.data.title;
                     console.log("Notif received !", notificationTitle);
@@ -92,8 +96,8 @@ function resolveFirebaseCloudMessagingToken() {
                     serviceWorkerRegistration: self.registration,
                     vapidKey: fbConfig.messaging.publicVapidKey
                 }).then(function(token) {
-                    firebaseMessagingToken = token;
                     console.info("Messaging token : ", token);
+                    firebaseMessagingToken = token;
                     return token;
                 }, console.error);
             })
@@ -102,46 +106,6 @@ function resolveFirebaseCloudMessagingToken() {
     }
 }
 
-function rechercherAbonnementsAvecRdvDispos(subscriptions) {
-    const subscriptionsByCodesDepartement = subscriptions.reduce(function(codeDepartements, subscription) {
-        codeDepartements.set(subscription.departement.code_departement, codeDepartements.get(subscription.departement.code_departement) || []);
-        codeDepartements.get(subscription.departement.code_departement).push(subscription);
-        return codeDepartements;
-    }, new Map())
-
-    return Promise.all(Array.from(subscriptionsByCodesDepartement.keys()).map(function(codeDepartement) {
-        return fetch(VMD_BASE_URL + "/" + codeDepartement + ".json")
-            .then(function(resp) { return resp.json(); })
-            .then(function(centres) { return { centres: centres, codeDepartement: codeDepartement }; })
-    })).then(function(results) {
-        return DB.instance().then(function(db) {
-            return db.transaction(['debug']).objectStore("debug").count()
-        }).then(function(count) { return { results: results, debugEnabled: count !== 0 }; });
-    }).then(function(_) {
-        const results = _.results;
-        const debugEnabled = _.debugEnabled;
-        return results.reduce(function(abosAvecRdvDispos, centresParDepartement) {
-            return subscriptionsByCodesDepartement.get(centresParDepartement.codeDepartement).reduce(function(abosAvecRdvDispos, subscription) {
-                const centre = centresParDepartement.centres.centres_disponibles.find(function(l) {
-                    // To be improved ??? With a levenshtein distance or something like this ? (in case lieu has been renamed a bit)
-                    return subscription.lieu.nom === l.nom;
-                });
-                if(centre && centre.appointment_count) {
-                    abosAvecRdvDispos.push({
-                        subscription: subscription,
-                        appointment_count: centre.appointment_count
-                    });
-                } else if(debugEnabled) {
-                    abosAvecRdvDispos.push({
-                        subscription: subscription,
-                        appointment_count: Math.round(Math.random()*100)
-                    });
-                }
-                return abosAvecRdvDispos;
-            }, abosAvecRdvDispos);
-        }, []);
-    });
-}
 
 class DB {
     static _INSTANCE = new DB();
