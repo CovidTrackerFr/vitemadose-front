69ad68752e6d85bb1378a68de871916e780a416c
Florent Jaby <florent.jaby@gmail.com>
2021-05-04 10:20:04 +0200

    Add conditional polyfills with vitejs legacy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
 import autoprefixer = require('autoprefixer')
-
 
 // https://vitejs.dev/config/
 export default ({ command, mode }) => ({
+  plugins: [
+    legacy()
+  ],
   build: {
     sourcemap: false
   },
