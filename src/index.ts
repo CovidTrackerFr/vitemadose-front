ea9f4fc1fde895c7d2ddcc94022d02609eaccf41
Joxit <joxit972@gmail.com>
2021-05-21 17:38:32 +0200

    feat: add tab with a map of all appointments in `vmd-rdv.view`

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import 'tippy.js/dist/tippy.css';
 import './vmd-app.component'
 import './components/vmd-search.component'
 import './components/vmd-appointment-card.component'
+import './components/vmd-appointment-map.component'
 import './components/vmd-appointment-metadata.component'
 import './components/vmd-commune-or-departement-selector.component'
 import './components/vmd-button-switch.component'
