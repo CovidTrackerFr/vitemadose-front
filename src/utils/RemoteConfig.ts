21b1cdd231c41c16c2d232d96a3c36a3a3cde8b4
fcamblor <fcamblor@gmail.com>
2021-08-05 21:28:08 +0200

    getting rid of firebase dependency, retrieving firebase remote config through manual REST APIs. This allows to remove a 340K => 940Ko increase in the production minified assets

--- src/utils/RemoteConfig.ts
+++ src/utils/RemoteConfig.ts
@@ -1,8 +1,3 @@
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-import firebase from 'firebase/app'
-
-// Add the Firebase products that you want to use
-import 'firebase/remote-config'
 
 type DataUrlGenerator = {
     listDepartements: () => string,
@@ -12,34 +7,28 @@ type DataUrlGenerator = {
     creneauxQuotidiensDepartement: (codeDepartement: string) => string,
 };
 
+type RemoteConfigEntries = {
+    "chronodose_min_count": string,
+    "data_disclaimer_enabled": string,
+    "data_disclaimer_message": string,
+    "data_disclaimer_repeat_days": string,
+    "data_disclaimer_severity": string,
+    "path_contributors": string,
+    "path_data_department": string,
+    "path_list_departments": string,
+    "path_stats": string,
+    "url_base": string,
+    "vaccination_centres_list_radius_in_km": string,
+};
+
 export class RemoteConfig {
     public static readonly INSTANCE = new RemoteConfig();
 
-    private readonly configuration: firebase.remoteConfig.RemoteConfig;
+    private configuration: RemoteConfigEntries|undefined = undefined;
     private _urlGenerator: DataUrlGenerator|undefined = undefined;
     private configurationSyncedPromise: Promise<void>|undefined = undefined;
 
     private constructor() {
-        const USE_PRODUCTION_REMOTE_CONFIG = RemoteConfig.currentEnv() === 'prod';
-        
-        const firebaseDevConfig = {
-            apiKey: "AIzaSyC5lncyBHo4HAmMecIvokok1A5PWWRrutw",
-            projectId: "vite-ma-dose-dev",
-          storageBucket: "vite-ma-dose-dev.appspot.com",
-          appId: "1:812389299998:web:ff949f4962d751b45dfb0f"
-        }
-        
-        const firebaseProdConfig = {
-          apiKey: "AIzaSyBl4_aecaPMtvy458zFbmDKu3rHfOZyaQU",
-          projectId: "vite-ma-dose",
-          storageBucket: "vite-ma-dose.appspot.com",
-          appId: "1:304644690082:web:e12d50228bc4493b25c7fb"
-        }
-        
-        const app = firebase.initializeApp(USE_PRODUCTION_REMOTE_CONFIG ? firebaseProdConfig : firebaseDevConfig)
-        this.configuration = firebase.remoteConfig(app);
-
-        this.configuration.settings.minimumFetchIntervalMillis = 60 * 60 * 1000; // one hour
     }
 
     public static currentEnv(): 'prod'|'testing'|'dev'|'unknown' {
@@ -55,44 +44,74 @@ export class RemoteConfig {
     }
     
     sync() {
-        this.configurationSyncedPromise = this.configuration.fetchAndActivate().then(() => {
-            // If false, rawgithub will be used
-            // but keep in mind that firebase remote config will be taken first
-            const USE_GITLAB_AS_FALLBACK = true;
+        const firebaseConfig = (RemoteConfig.currentEnv() === 'prod')?{
+            apiKey: "AIzaSyBl4_aecaPMtvy458zFbmDKu3rHfOZyaQU",
+            projectId: "vite-ma-dose",
+            appId: "1:304644690082:web:e12d50228bc4493b25c7fb"
+        }:{
+            apiKey: "AIzaSyC5lncyBHo4HAmMecIvokok1A5PWWRrutw",
+            projectId: "vite-ma-dose-dev",
+            appId: "1:812389299998:web:ff949f4962d751b45dfb0f"
+        };
 
-            let urlBase = this.configuration.getString("url_base");
-            if(urlBase) {
-                const statsPath = this.configuration.getString("path_stats") || `/vitemadose/stats.json`;
-                const statsByDatePath = '/' + (this.configuration.getString("path_stats_by_date") || `vitemadose/stats_by_date.json`);
-                const departementsListPath = this.configuration.getString("path_list_departments") || `/vitemadose/departements.json`;
-                const infosDepartementPath = this.configuration.getString("path_data_department") || `/vitemadose/{code}.json`;
-                this._urlGenerator = {
-                    listDepartements: () => `${urlBase}${departementsListPath}`,
-                    statsByDate: () => `${urlBase}${statsByDatePath}`,
-                    stats: () => `${urlBase}${statsPath}`,
-                    infosDepartement: (codeDepartement) => `${urlBase}${infosDepartementPath.replace('{code}', codeDepartement)}`,
-                    creneauxQuotidiensDepartement: (codeDepartement) => `${urlBase}/vitemadose/${codeDepartement}/creneaux-quotidiens.json`
-                };
-            } else if(USE_GITLAB_AS_FALLBACK) {
-                this._urlGenerator = {
-                    listDepartements: () => 'https://vitemadose.gitlab.io/vitemadose/departements.json',
-                    statsByDate: () => `https://vitemadose.gitlab.io/vitemadose/stats_by_date.json`,
-                    stats: () => `https://vitemadose.gitlab.io/vitemadose/stats.json`,
-                    infosDepartement: (codeDepartement) => `https://vitemadose.gitlab.io/vitemadose/${codeDepartement}.json`,
-                    creneauxQuotidiensDepartement: (codeDepartement) => `https://vitemadose.gitlab.io/vitemadose/${codeDepartement}/creneaux-quotidiens.json`
-                };
-            } else {
-                this._urlGenerator = {
-                    listDepartements: () => `https://raw.githubusercontent.com/CovidTrackerFr/vitemadose/data-auto/data/output/departements.json`,
-                    statsByDate: () => `https://raw.githubusercontent.com/CovidTrackerFr/vitemadose/data-auto/data/output/stats_by_date.json`,
-                    stats: () => `https://raw.githubusercontent.com/CovidTrackerFr/vitemadose/data-auto/data/output/stats.json`,
-                    infosDepartement: (codeDepartement) => `https://raw.githubusercontent.com/CovidTrackerFr/vitemadose/data-auto/data/output/${codeDepartement}.json`,
-                    creneauxQuotidiensDepartement: (codeDepartement) => `https://raw.githubusercontent.com/CovidTrackerFr/vitemadose/data-auto/data/output/${codeDepartement}/creneaux-quotidiens.json`
-                };
-            }
+        this.configurationSyncedPromise = fetch(`https://firebaseinstallations.googleapis.com/v1/projects/${firebaseConfig.projectId}/installations`, {
+            method: 'POST',
+            headers: {
+                'x-goog-api-key': firebaseConfig.apiKey
+            },
+            body: JSON.stringify({
+                "appId": firebaseConfig.appId,
+                "sdkVersion":"w:0.4.30"
+            })
+        }).then(resp => resp.json())
+          .then(installation => fetch(`https://firebaseremoteconfig.googleapis.com/v1/projects/${firebaseConfig.projectId}/namespaces/firebase:fetch?key=${firebaseConfig.apiKey}`, {
+              method: 'POST',
+              body: JSON.stringify({
+                "app_instance_id": installation.fid,
+                "app_id": firebaseConfig.appId
+              })
+          })).then(resp => resp.json())
+            .then(remoteConfig => {
+                this.configuration = remoteConfig.entries as RemoteConfigEntries;
 
-            return undefined as void;
-        });
+                // If false, rawgithub will be used
+                // but keep in mind that firebase remote config will be taken first
+                const USE_GITLAB_AS_FALLBACK = true;
+
+                let urlBase = this.configuration.url_base;
+                if(urlBase) {
+                    const statsPath = this.configuration.path_stats || `/vitemadose/stats.json`;
+                    const statsByDatePath = `/vitemadose/stats_by_date.json`;
+                    const departementsListPath = this.configuration.path_list_departments || `/vitemadose/departements.json`;
+                    const infosDepartementPath = this.configuration.path_data_department || `/vitemadose/{code}.json`;
+                    this._urlGenerator = {
+                        listDepartements: () => `${urlBase}${departementsListPath}`,
+                        statsByDate: () => `${urlBase}${statsByDatePath}`,
+                        stats: () => `${urlBase}${statsPath}`,
+                        infosDepartement: (codeDepartement) => `${urlBase}${infosDepartementPath.replace('{code}', codeDepartement)}`,
+                        creneauxQuotidiensDepartement: (codeDepartement) => `${urlBase}/vitemadose/${codeDepartement}/creneaux-quotidiens.json`
+                    };
+                } else if(USE_GITLAB_AS_FALLBACK) {
+                    this._urlGenerator = {
+                        listDepartements: () => 'https://vitemadose.gitlab.io/vitemadose/departements.json',
+                        statsByDate: () => `https://vitemadose.gitlab.io/vitemadose/stats_by_date.json`,
+                        stats: () => `https://vitemadose.gitlab.io/vitemadose/stats.json`,
+                        infosDepartement: (codeDepartement) => `https://vitemadose.gitlab.io/vitemadose/${codeDepartement}.json`,
+                        creneauxQuotidiensDepartement: (codeDepartement) => `https://vitemadose.gitlab.io/vitemadose/${codeDepartement}/creneaux-quotidiens.json`
+                    };
+                } else {
+                    this._urlGenerator = {
+                        listDepartements: () => `https://raw.githubusercontent.com/CovidTrackerFr/vitemadose/data-auto/data/output/departements.json`,
+                        statsByDate: () => `https://raw.githubusercontent.com/CovidTrackerFr/vitemadose/data-auto/data/output/stats_by_date.json`,
+                        stats: () => `https://raw.githubusercontent.com/CovidTrackerFr/vitemadose/data-auto/data/output/stats.json`,
+                        infosDepartement: (codeDepartement) => `https://raw.githubusercontent.com/CovidTrackerFr/vitemadose/data-auto/data/output/${codeDepartement}.json`,
+                        creneauxQuotidiensDepartement: (codeDepartement) => `https://raw.githubusercontent.com/CovidTrackerFr/vitemadose/data-auto/data/output/${codeDepartement}/creneaux-quotidiens.json`
+                    };
+                }
+
+                return undefined as void;
+            })
+
         return this.configurationSyncedPromise;
     }
 
@@ -103,17 +122,17 @@ export class RemoteConfig {
 
     async disclaimerEnabled() {
         await this.configurationSyncedPromise;
-        return this.configuration.getBoolean('data_disclaimer_enabled');
+        return this.configuration!.data_disclaimer_enabled === 'true';
     }
     
     async disclaimerMessage() {
         await this.configurationSyncedPromise;
-        return this.configuration.getString('data_disclaimer_message');
+        return this.configuration!.data_disclaimer_message;
     }
 
     async disclaimerSeverity() {
         await this.configurationSyncedPromise;
-        return this.configuration.getString('data_disclaimer_severity').toLowerCase() as DisclaimerSeverity;
+        return this.configuration!.data_disclaimer_severity.toLowerCase() as DisclaimerSeverity;
     }
 }
 
